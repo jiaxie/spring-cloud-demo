60d7b864766e7ea480257e53d2280966a65e06f7
Yingying Chen <chenyybamboo@gmail.com>
2018-03-05 08:50:32 +0800

    Tagging docker image to latest

--- docker-images/build.sh
+++ docker-images/build.sh
@@ -13,3 +13,10 @@ docker_tag="1.$2"
 print_status "Building docker image: ${docker_repository}:${docker_tag}"
 docker build --no-cache -t "${docker_repository}:${docker_tag}" "${dockerfile_dir}"
 
+print_status "Tagging docker image"
+docker tag "${docker_repository}:${docker_tag}" "${docker_repository}:latest"
+if [ $? -ne 0 ]; then
+  print_status "${prog}: Error: Failed to tag the latest docker image (${docker_repository}:${docker_tag})"
+  exit 1
+fi
+
